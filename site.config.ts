4dfbd43f418b7c5f0f5dba5a0ed255caa2aa9847
Apoorva Singh <apoorvasingh157@gmail.com>
2022-05-12 01:26:45 +0530

    The site is now working
    npm run dev is not throwing anymore HTTP 403 Forbidden error
    when it tries to fetch a page from Notion. I don't know how it got
    fixed but atleast I'm not seeing the error anymore.

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,26 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  // https://apoorvas.notion.site/Testing-Blog-3586902c77054fa486533083b2350229
+  // https://apoorvas.notion.site/Next-js-Notion-Starter-Kit-Template-983badf4e7c84fb4a3dcc22d9d55f5c4
+  rootNotionPageId: '983badf4e7c84fb4a3dcc22d9d55f5c4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Solving for Future',
+  domain: 'localhost',
+  author: 'Apoorva Singh',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'My notes on how things work.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'apoorsin',
+  github: 'meticulousCraftman',
+  linkedin: 'apoorsin',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -44,6 +46,7 @@ export default siteConfig({
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
   pageUrlOverrides: null,
+  // includeNotionIdInUrls: true,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
